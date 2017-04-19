3715dd15961d6297433586e59cd0d2579c80d407
Philipdev95 <philipdev@icloud.com>
2017-04-19 16:42:50 +0200

    Bootstrap on sidemenu
    and js fix

--- matentus/static/js/script.js
+++ matentus/static/js/script.js
@@ -1,30 +1,25 @@
+function showMenu() {
+    var menu = document.getElementById("side_menu"),
+        menu_icon = document.getElementById("menu_btn");
+    if (menu.style.display == "block") {
+        menu.style.display = "none";
 
-
-function showMenu(){
-	var menu = document.getElementById("side_menu")
-    var menu_icon = document.getElementById("menu_btn")
-	if(menu.style.display == "block"){
-		menu.style.display = "none";
-        
-	}else{
-		menu.style.display = "block";
+    } else {
+        menu.style.display = "block";
         menu_icon.style.display = "none";
-	}  
-    console.log("hej")
+    }
+    console.log("hej");
 }
-document.getElementById("menu_btn").addEventListener("click", showMenu);
+$("#menu_btn").click(showMenu);
 
 
-function hideMenu(){
-	var menu = document.getElementById("side_menu")
-    var menu_icon = document.getElementById("menu_btn")
+function hideMenu() {
+    var menu = document.getElementById("side_menu"),
+        menu_icon = document.getElementById("menu_btn");
     menu.style.display = "none";
     menu_icon.style.display = "";
-    
-	}
-   
-document.getElementById("close_btn").addEventListener("click", hideMenu);
 
+}
 
-
-
+$("#close_btn").click(hideMenu);
+$(".category").click(hideMenu);
