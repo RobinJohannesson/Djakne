4f0968f3b3adf686ca09c1afbdd7c18f9c1cba82
Philipdev95 <philipdev@icloud.com>
2017-04-21 12:04:02 +0200

    makeprettybuttons

--- matentus/static/js/script.js
+++ matentus/static/js/script.js
@@ -1,14 +1,9 @@
 function showMenu() {
     var menu = document.getElementById("side_menu"),
         menu_icon = document.getElementById("burger");
-    if (menu.style.display == "block") {
-        menu.style.display = "none";
 
-    } else {
-        menu.style.display = "block";
-        menu_icon.style.display = "none";
-    }
-    console.log("hej");
+    menu.style.display = "block";
+    menu_icon.style.transform = "scale(0)";
 }
 $("#burger").click(showMenu);
 
@@ -17,9 +12,7 @@ function hideMenu() {
     var menu = document.getElementById("side_menu"),
         menu_icon = document.getElementById("burger");
     menu.style.display = "none";
-    menu_icon.style.display = "";
-
+    menu_icon.style.transform = "scale(1)";
 }
-
 $("#close_btn").click(hideMenu);
 $(".category").click(hideMenu);
