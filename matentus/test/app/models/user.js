8fc44cc73381134e32e574e1a6cb49273a664096
Oskar Katra <oskar1.katra@gmail.com>
2017-04-26 13:36:44 +0200

    login test

---
+++ matentus/test/app/models/user.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING,
+    fbid: DataTypes.STRING,
+    googleid: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+      }
+    }
+  });
+
+  return User;
+};
---
+++ matentus/test/app/routes/Namnlös-1.js
@@ -0,0 +1,152 @@
+var express = require('express');
+var router = express.Router();
+var models  = require('../models');
+var _ = require("lodash");
+var express = require("express");
+var bodyParser = require("body-parser");
+var jwt = require('jsonwebtoken');
+var models  = require('../models');
+
+var passport = require("passport");
+var passportJWT = require("passport-jwt");
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+
+var jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeader();
+jwtOptions.secretOrKey = 'tasmanianDevil';
+
+var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+  var user =  models.User.find( {
+    where: {id: jwt_payload.id}
+  })
+  .then(function(user) {
+    if (user) {
+      next(null, user);
+    } else {
+      next(null, false);
+    }
+  });
+
+});
+
+passport.use(strategy);
+
+var app = express();
+app.use(passport.initialize());
+
+// parse application/x-www-form-urlencoded
+// for easier testing with Postman or plain HTML forms
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get("/", function(req, res) {
+  res.json({message: "Express is up!"});
+});
+
+app.post("/login", function(req, res) {
+  if(req.body.name && req.body.password){
+    var name = req.body.name;
+    var password = req.body.password;
+  }
+  // usually this would be a database call:
+  var user = models.User.find( {
+    where: {username: name}
+  })
+  .then(function(user) {
+    if (!user) {
+      res.status(401).json({message:"no such user found"});;
+    } 
+    if(user.password === req.body.password) {
+      // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
+      var payload = {id: user.id};
+      var token = jwt.sign(payload, jwtOptions.secretOrKey);
+      res.json({message: "ok", token: token});
+    } else {
+      res.status(401).json({message:"passwords did not match"});
+    } 
+  });
+});
+
+app.get("/secret", passport.authenticate('jwt', { session: false }), function(req, res){
+  res.json({message: "Success! You can not see this without a token"});
+});
+
+app.get("/secretDebug",
+        function(req, res, next){
+  console.log(req.get('Authorization'));
+  next();
+}, function(req, res){
+  res.json("debugging");
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/userss', function(req, res) {
+  models.User.findAll({
+  }).then(function(users) {
+    res.json(users)
+    });
+  });
+
+passport.use(new FacebookStrategy({
+    clientID: "1960319234199009",
+    clientSecret: "c56e85a4ea9a2b98303a6518d882b417",
+    callbackURL: "http://localhost:3000/auth/facebook/callback",
+    profileFields: ['id', 'displayName', 'photos', 'email']
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    var user = models.User.find( {
+    where: {fbid: profile.id}
+  })
+  .then(function(user) {
+    if (!user) {
+     var userX = models.User.create({ username: profile.displayName, email: profile._json.email, password: null , fbid: profile.id}).then(function(err, userX){return cb(err,userX)});
+  
+    } 
+    if(user) {
+     var payload = {id: profile.id};
+     var token = jwt.sign(payload, jwtOptions.secretOrKey);
+     console.log(token);
+    } else {
+      //res.json({message:"passwords did not match"});
+    } 
+  });
+  }
+));
+
+
+app.get('/auth/facebook', passport.authenticate('facebook',{session: false, scope: ['email']}));  
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {session: false, failureRedirect: '/test' }),
+  function(req, res) {
+    console.log("TESTTESTTESTTEST")
+    var payload = {id: profile.id};
+    var token = jwt.sign(payload, jwtOptions.secretOrKey);
+    res.json(token);
+    res.redirect('/');
+  });
+
+router.get('/test'), function(req, res){
+  console.log("/test");
+}
+
+
+module.exports = router;
+
+app.listen(3000, function() {
+  console.log("Express running");
+});
+
