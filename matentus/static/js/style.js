d9608359a67638edcf36f8837a8500ed463becc9
Philipdev95 <philipdev@icloud.com>
2017-04-26 14:59:34 +0200

    Email registrering

--- matentus/static/js/style.js
+++ matentus/static/js/style.js
@@ -7,3 +7,8 @@ $("#kat-div").slideUp();
 $("#kategori").click(function () {
     $("#kat-div").slideToggle();
 });
+$("#email-form-div").slideUp();
+$("#email-btn").click(function () {
+    $("#email-btn").toggleClass("color-grey color-white");
+    $("#email-form-div").slideToggle();
+});
