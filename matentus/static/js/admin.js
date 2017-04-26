10a918e8574ff384070c6004550dbe41902713c2
Agnes Broomé Clason <agnesbroomeclason@agnes-air.clients.internal.mah.se>
2017-04-26 13:33:54 +0200

    Update admin

--- matentus/static/js/admin.js
+++ matentus/static/js/admin.js
@@ -34,6 +34,7 @@ $(document).ready(function(){
     });
 });
 
+//below follows lines of code for showing the right kind of form when adding new category. 
 $(document).ready(function(){
     $("#new_category").click(function(){
         $(".cat_radio_choice").css("display", "block");
@@ -41,12 +42,35 @@ $(document).ready(function(){
     });
 });
 
-/*Till nästa gång: rätt div ska visas när man väljer typ av kategori i radio buttons*/
+$(document).ready(function(){
+    $("#main_cat_choice").click(function(){
+        $(".main_cat_div").css("display", "block");
+        $(".sub_cat_div").css("display", "none");
+    });
+});
 
+$(document).ready(function(){
+    $("#sub_cat_choice").click(function(){
+        $(".sub_cat_div").css("display", "block");
+        $(".main_cat_div").css("display", "none");
+    });
+});
 
+//controls the menu marking. 
 $(".admin_btn").click(function () {
     $(".admin_btn").removeClass("active");
     $(this).addClass("active");
 });
 
+ function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
 
+                reader.onload = function (e) {
+                    $('#img_preview')
+                        .attr('src', e.target.result);
+                };
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
