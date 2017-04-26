bbf4f77c671b8711afb8a9330ce2be5eaf26754e
Kajsa Ornstein <kajsaorn@gmail.com>
2017-04-26 12:51:25 +0200

    add angular project folder

---
+++ matentus/api/routes/register.js
@@ -0,0 +1,5 @@
+// -----------------------------------------------------
+// Require modules and setup router
+// -----------------------------------------------------
+var express = require('express');
+var router = express.Router();
