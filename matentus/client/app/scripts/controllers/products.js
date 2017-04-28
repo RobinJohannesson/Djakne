fb4ea3f3e30e80e3638a981de02765fe33539f7a
Agnes Broomé Clason <agnesbroomeclason@Agnes-MacBook-Air.local>
2017-04-28 15:53:12 +0200

    Get categories

--- matentus/client/app/scripts/controllers/products.js
+++ matentus/client/app/scripts/controllers/products.js
@@ -1,38 +1,25 @@
 
 // ------------------------------------------------------------
-// 	Products Controller
+//  Products Controller
 // ------------------------------------------------------------
-
 'use strict';
-
 angular.module('matentusApp')
-	.controller('ProductsCtrl', ProductsCtrl);
-
+    .controller('ProductsCtrl', ProductsCtrl);
 // ------------------------------------------------------------
-// 	This controller has access to "services/httpService.js"
+//  This controller has access to "services/httpService.js"
 // ------------------------------------------------------------
-
-ProductsCtrl.$inject = ['productService'];
-
+ProductsCtrl.$inject = ['productService', 'categoryService'];
 // ------------------------------------------------------------
-// 	Variables and functions available to "views/products.html"
+//  Variables and functions available to "views/products.html"
 // ------------------------------------------------------------
-
-function ProductsCtrl(productService) {
+function ProductsCtrl(productService, categoryService) {
   var ctrl = this;
-
   
-
+  ctrl.categories = categoryService.categories;
   ctrl.shouldShowCategories = false;
   ctrl.toggleCategories = toggleCategories;
-
   function toggleCategories() {
-  	ctrl.shouldShowCategories = ctrl.shouldShowCategories ? false : true;
-  	console.log(ctrl.shouldShowCategories);
+    ctrl.shouldShowCategories = ctrl.shouldShowCategories ? false : true;
+    console.log(ctrl.shouldShowCategories);
   }
-
 }
-
-
-
-
