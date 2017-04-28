5df56196bdb09c12cb9b1afd777d41d5ef18d298
Agnes Broomé Clason <agnesbroomeclason@Agnes-MacBook-Air.local>
2017-04-28 13:27:07 +0200

    Rewrite css-code in index1.

---
+++ matentus/client/app/scripts/controllers/product.js
@@ -0,0 +1,25 @@
+
+// ------------------------------------------------------------
+// 	Product Controller
+// ------------------------------------------------------------
+
+'use strict';
+
+angular.module('matentusApp')
+	.controller('ProductCtrl', ProductCtrl);
+
+// ------------------------------------------------------------
+// 	This controller has access to "services/httpService.js"
+// ------------------------------------------------------------
+
+// ProductCtrl.$inject = [''];
+
+// ------------------------------------------------------------
+// 	Variables and functions available to "views/product.html"
+// ------------------------------------------------------------
+
+function ProductCtrl() {
+  var ctrl = this;
+  // ctrl.test = httpService.categories;
+}
+
