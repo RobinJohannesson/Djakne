bc9fb42f8c8758ded3e78b0d9ac5672c481001d3
Agnes Broomé Clason <agnesbroomeclason@Agnes-MacBook-Air.local>
2017-04-28 11:21:12 +0200

    Menu fix angular

--- matentus/client/app/scripts/styling/script.js
+++ matentus/client/app/scripts/styling/script.js
@@ -10,25 +10,28 @@
 
 
 
-// function showMenu() {
-//     var menu = document.getElementById("side_menu"),
-//         menu_icon = document.getElementById("burger");
+// 
+/*
+function showMenu() {
+     var menu = document.getElementById("side_menu"),
+         menu_icon = document.getElementById("burger");
 
-//     menu.style.display = "block";
-//     menu_icon.style.transform = "scale(0)";
-// }
+     menu.style.display = "block";
+     menu_icon.style.transform = "scale(0)";
+ }
 
-// $("#burger").click(showMenu);
-
-
-// function hideMenu() {
-//     var menu = document.getElementById("side_menu"),
-//         menu_icon = document.getElementById("burger");
-//     menu.style.display = "none";
-//     menu_icon.style.transform = "scale(1)";
-// }
-
-// $("#close_btn").click(hideMenu);
-// $(".category").click(hideMenu);
+ $("#burger").click(showMenu);
 
 
+ function hideMenu() {
+     var menu = document.getElementById("side_menu"),
+         menu_icon = document.getElementById("burger");
+     menu.style.display = "none";
+     menu_icon.style.transform = "scale(1)";
+ }
+
+ $("#close_btn").click(hideMenu);
+ $(".category").click(hideMenu);
+
+
+*/
