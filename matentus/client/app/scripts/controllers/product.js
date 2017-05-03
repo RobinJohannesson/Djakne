0350c2b30a1029c9cb7cc30e27cc5857f7af7653
RobinJohannesson <robinjohannesson96@gmail.com>
2017-05-03 13:50:43 +0200

    uppladdning av produkter

--- matentus/client/app/scripts/controllers/product.js
+++ matentus/client/app/scripts/controllers/product.js
@@ -12,17 +12,27 @@ angular.module('matentusApp')
 // 	This controller has access to "services/httpService.js"
 // ------------------------------------------------------------
 
-// ProductCtrl.$inject = [''];
+ ProductCtrl.$inject = ['$scope', '$http', '$routeParams'];
 
 // ------------------------------------------------------------
 // 	Variables and functions available to "views/product.html"
 // ------------------------------------------------------------
 
-function ProductCtrl() {
+function ProductCtrl($scope, $http) {
   var ctrl = this;
+    ctrl.postProduct = postProduct;
   // ctrl.test = httpService.categories;
   function postProduct(){
-    alert("funkar");  
+      console.log("test");
+      console.log($scope);
+      $http({
+          method: 'POST',
+          url: 'http://localhost:3000/api/products/postproduct',
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+          data: {title: $scope.title, description: $scope.desc, subcategory: 1}
+      })
+      .then(function(response){
+          console.log("testarrrr");
+      })
   }
 }
-
