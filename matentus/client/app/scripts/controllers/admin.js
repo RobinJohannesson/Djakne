8bbb9be6620bd223edce4acc563a7fefc881fecd
Agnes Broomé Clason <agnesbroomeclason@agnes-air.clients.internal.mah.se>
2017-05-03 15:04:41 +0200

    Try to fix admin

---
+++ matentus/client/app/scripts/controllers/admin.js
@@ -0,0 +1,76 @@
+
+// ------------------------------------------------------------
+// 	Admin Controller
+// ------------------------------------------------------------
+
+'use strict';
+
+angular.module('matentusApp')
+	.controller('AdminCtrl', AdminCtrl);
+
+// ------------------------------------------------------------
+// 	This controller has access to "services/httpService.js"
+// ------------------------------------------------------------
+
+ AdminCtrl.$inject = ['$scope'];
+
+// ------------------------------------------------------------
+// 	Variables and functions available to "views/admin.html"
+// ------------------------------------------------------------
+
+function AdminCtrl($scope) {
+    $scope = $scope;
+    var ctrl = this;
+    ctrl.postAdmin = postAdmin;
+  // ctrl.test = httpService.categories;
+    function postAdmin(){
+        alert("funkar");  
+  };
+};
+
+
+
+/*
+
+
+$scope.one = true; // setting the first div visible when the page loads
+$scope.two = false; // hidden
+$scope.three = false; // hidden
+
+// Now have three functions that change the ng-show based on the click
+$scope.showOne = function (){
+  $scope.one = true;
+  $scope.two = false;
+  $scope.three = false;
+}
+
+$scope.showTwo = function (){
+  $scope.one = false;
+  $scope.two = true; // now show this one
+  $scope.three = false;
+    console.log("her")
+}
+
+$scope.requests = function($scope){
+    console.log("hej")
+};
+
+*/
+/*
+$scope.addProduct = function($scope){
+    $scope.view = '/views/addProduct.html';
+};
+
+function changeProduct(){
+    $("#admin_manage_product").css("display", "block");
+    $("#admin_request").css("display", "none");
+    $("#admin_categories").css("display", "none");
+    $("#admin_add_product").css("display", "none");
+};
+function manageCategory(){
+    $("#admin_categories").css("display", "block");
+    $("#admin_request").css("display", "none");
+    $("#admin_manage_product").css("display", "none");
+    $("#admin_add_product").css("display", "none");
+};*/
+
