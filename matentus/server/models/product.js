2dde910120de7df608dc7ca1477ccb82badc2d32
Oskar Katra <oskar1.katra@gmail.com>
2017-05-10 14:13:29 +0200

    fix-supplier

--- matentus/server/models/product.js
+++ matentus/server/models/product.js
@@ -18,10 +18,10 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
-    supplier: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
+//    supplier: {
+//      type: DataTypes.STRING,
+//      allowNull: true
+//    },
     rating: {
       type: DataTypes.INTEGER(1),
       allowNull: false
