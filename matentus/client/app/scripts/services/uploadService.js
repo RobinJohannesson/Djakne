8ee87805f38b7e2bd5554fe7c65a630202328efb
Kajsa Ornstein <kajsaorn@gmail.com>
2017-05-12 11:35:58 +0200

    upload image and get image OK

--- matentus/client/app/scripts/services/uploadService.js
+++ matentus/client/app/scripts/services/uploadService.js
@@ -11,7 +11,7 @@
 
       
   	function upload(product) {
-  		var url = 'http://localhost:3000/api/products/upload';
+  		var url = 'http://localhost:3000/api/products';
   		var formData = new FormData();
   		for(var key in product) {
   			formData.append(key, product[key]);
