9e5654d14a622074a149ae5d5fa7f1ae8cad57ef
Oskar Katra <oskar1.katra@gmail.com>
2017-05-15 00:45:55 +0200

    Facebook-Login Backend (ej klar!)
    npm install behövs.

--- matentus/server/controllers/loginController.js
+++ matentus/server/controllers/loginController.js
@@ -8,7 +8,7 @@ var passportJWT = require("passport-jwt");
 var jwt = require('jsonwebtoken');
 var ExtractJwt = passportJWT.ExtractJwt;
 var JwtStrategy = passportJWT.Strategy;
-
+var request = require('request');
 
 var jwtOptions = {}
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeader();
@@ -32,25 +32,50 @@ var authenticate = new Promise(function() {
 });
 
 module.exports = {
-
+	//TODO:Fixa så vi får ut email från vår access-token.
 	fblogin: 	function(req, res) {
-		var fbtoken = req.body.fbtoken;
-		// authenticate to facebook api
+		var fbtoken = req.params.token;
+		request.get({ url: 'https://graph.facebook.com/me?access_token='+fbtoken },      function(error, response, body) { 
+			if (!error && response.statusCode == 200) {
+				var info =JSON.parse(response.body);
+				email=info['email'];
 
+				var user = models.User.find( {
+					where: {email: email}
+				})
+				.then(function(user) {
+					if (!user) {
+						models.User.create({name: info['name'], email: info['email'], admin: 0});
+						var user = models.User.find( {
+							where: {email: req.body.email}
+						})
+						.then(function() {
+							var payload = {id: user.id};
+							var token = jwt.sign(payload, jwtOptions.secretOrKey);
+							res.json({status: 0, token: token});
+						})
+						if(user) {
+							var payload = {id: user.id};
+							var token = jwt.sign(payload, jwtOptions.secretOrKey);
+							res.json({status: 0, token: token});
+						} 
+					};
+				}) 
+				}
+		})
 	},
-
 	googlelogin: 	function(req, res) {
 
 		// authenticate to google api
 
 	},
-	
+
 	checkLoginStatus: 	function(req, res) {
 
-		// authenticate to google api
+		// Return status OK is Matentus-token is valid.
 
 	},
-	
+
 	/*
 	Local Login
 	Status, 0 : User authenticated, token returned.
@@ -78,7 +103,7 @@ module.exports = {
 			} 
 		});
 	},
-	
+
 	/*
 	Create Local User
 	Status, 0 : User created, token returned.
@@ -92,7 +117,7 @@ module.exports = {
 		})
 		.then(function(user) {
 			if (user){
-				res.jsonjson({status:1});
+				res.json({status:1});
 			}
 			else if (!user) {
 				models.User.create({name: req.body.name, email: req.body.email, password: hashedPassword, admin: 0});
@@ -104,7 +129,7 @@ module.exports = {
 					var token = jwt.sign(payload, jwtOptions.secretOrKey);
 					res.json({status: 0, token: token});
 				})
-			}
+				}
 		});
 	}
 }
