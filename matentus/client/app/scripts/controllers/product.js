293b7ae26a676cac11eaa00e203a4566d33c97dc
RobinJohannesson <robinjohannesson96@gmail.com>
2017-05-17 13:32:30 +0200

    försök till delning av produkt

--- matentus/client/app/scripts/controllers/product.js
+++ matentus/client/app/scripts/controllers/product.js
@@ -7,7 +7,7 @@
 	'use strict';
 
 	angular.module('matentusApp')
-	.controller('ProductCtrl', ProductCtrl);
+	.controller('ProductCtrl', ProductCtrl, 'Socialshare');
 
 	ProductCtrl.$inject = ['$routeParams', 'productService'];
 
@@ -31,7 +31,14 @@
 		 function filterRelated(product) {
 		 	return product.supplier === ctrl.product.supplier || product.keyword === ctrl.product.keyword;
 		 }
-		
+		function testController(Socialshare){
+            Socialshare.share({
+            'provider': 'facebook',
+            'attrs': {
+            'socialshareUrl': 'http://google.com'
+            }
+            })
+        }
 	}
 
 })();
