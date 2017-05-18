60a61f747c4b603f98469d8aef27c63c5d88c535
RobinJohannesson <robinjohannesson96@gmail.com>
2017-05-18 12:45:58 +0200

    flyttade script

--- matentus/client/app/scripts/controllers/product.js
+++ matentus/client/app/scripts/controllers/product.js
@@ -35,6 +35,7 @@
 				}
 		    	
 		    }); 
+            facebookshare();
 		 }
 
 		 function filterRelated(product) {
@@ -47,11 +48,12 @@
 		}
         
         function facebookshare(){
-            (function(d, s, id) {
+            console.log("testar loss");
+            (function(d, s, id){
                 var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
+                if (d.getElementById(id)) {return;}
                 js = d.createElement(s); js.id = id;
-                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
+                js.src = "//connect.facebook.net/en_US/sdk.js";
                 fjs.parentNode.insertBefore(js, fjs);
             }(document, 'script', 'facebook-jssdk'));
         }
