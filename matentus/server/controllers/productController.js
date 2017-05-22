3320c1db5324eafa09909ebdd33f806ec390d7d5
Oskar Katra <oskar1.katra@gmail.com>
2017-05-22 09:55:58 +0200

    client csv-file (påbörjad)

--- matentus/server/controllers/productController.js
+++ matentus/server/controllers/productController.js
@@ -72,10 +72,12 @@ module.exports = {
 		.then(function(isAdmin) {
 			if(!isAdmin) {
 				res.sendStatus(status.NOT_ADMIN);
-				return;
+				const err = new Error('Not Admin');
+				throw(err);
 			}
 		})
 		.then(function() {
+			console.log("error2");
 			models.Product.findAll({
 				where: {
 					approved: false
